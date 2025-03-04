3e808e5588744f571dec5d367d9ffa53eaa185c0
Arthur Edelstein <arthuredelstein@gmail.com>
2025-03-04 00:07:33 -0800

    build-ghpages

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,12 @@
-export const config = {
-  dir: {
-    input: "src"
-  }
+export default function (eleventyConfig) {
+  // Add a passthrough copy for static files (like images)
+  eleventyConfig.addPassthroughCopy("src/images");
+
+  // Return the configuration object
+  return {
+    dir: {
+      input: "src",  // Tell Eleventy to use the "src" folder as the input
+      output: "_site",  // The output folder where Eleventy will generate static files
+    }
+  };
 };
