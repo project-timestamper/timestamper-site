e13a5455ef71311ccb24bae53a3825ecaec16219
Arthur Edelstein <arthuredelstein@gmail.com>
2025-03-09 17:32:57 -0700

    control name sorting

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,14 @@
+
+function sortByNameReverse(values) {
+  return values.slice().sort((a, b) => a.data.name.localeCompare(b.data.name)).reverse()
+}
+
 export default function (eleventyConfig) {
   // Add a passthrough copy for static files (like images)
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/styles");
+  // Add a sorting filter.
+  eleventyConfig.addFilter("sortByNameReverse", sortByNameReverse);
   // Return the configuration object
   return {
     dir: {
