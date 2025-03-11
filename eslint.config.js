7b8c48b9db1113633000f94dd6e5484379a6a81e
Arthur Edelstein <arthuredelstein@gmail.com>
2025-03-11 07:42:38 -0700

    separate out template for homepage

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import html from '@html-eslint/eslint-plugin'
+import frontmatter from 'eslint-plugin-frontmatter'
+
+export default [
+  // your own configurations.
+  {
+    plugins: {
+      frontmatter
+    },
+    rules: {
+      "@html-eslint/indent": ["error", 2],
+      "@html-eslint/no-trailing-spaces": "error"
+    },
+  },
+  {
+    // recommended configuration included in the plugin
+    ...html.configs['flat/recommended'],
+    files: ['**/*.html']
+  }
+]
